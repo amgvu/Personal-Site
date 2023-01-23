a60449c6cc90d08f491d66d3992ea311f013b1c6
Kevin Vu <mingleekevin@gmail.com>
2023-01-22 18:08:44 -0600

    node_modules gotta go

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e93a947e8bd96b7"; }
+/******/ 		__webpack_require__.h = function() { return "3aa112fbe19a6992"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
