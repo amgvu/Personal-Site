f341b27c3cbdd504b63d9d329d53111508ccfc16
amgvu <mingleekevin@gmail.com>
2024-04-12 06:33:55 -0500

    hibbidybibbidy

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ export default function Home() {
         <section className="flex min-h-screen flex-grow flex-col place-content-center">
           <nav className="mb-8 font-sans flex justify-end py-4 md:py-8 text-gray-200">
             <div className="ease-in-out animate__fadeIn animate__delay-1s animate__animated font-bold">
-            <RadioComponent text="130.2 TREATMENT FM" url="https://soundcloud.com/differentsoundofficial/ds-premiere-urquiaga-backup"/>
+            <RadioComponent text="TREATMENT 130.2 FM" url="https://soundcloud.com/differentsoundofficial/ds-premiere-urquiaga-backup"/>
             </div>
             <h1 className="font-miracle text-xl"></h1>
             <ul className="flex items-center">
