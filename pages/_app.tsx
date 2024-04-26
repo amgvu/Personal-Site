46fc7fca008d9fa96aab7e23f284638139046027
amgvu <mingleekevin@gmail.com>
2024-04-25 20:19:51 -0500

    loading time

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     useEffect(() => {
         const timer = setTimeout(() => {
             setLoading(false);
-        }, 1500);
+        }, 2500);
         return () => clearTimeout(timer);
     }, []);
 
