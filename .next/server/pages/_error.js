266813f1cfc83ccf26ce92864553223dfa5051df
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-01-27 10:46:25 -0600

    lock file

--- .next/server/pages/_error.js
+++ .next/server/pages/_error.js
@@ -1,6 +1,6 @@
 const CHUNK_PUBLIC_PATH = "server/pages/_error.js";
 const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_879b31._.js");
-runtime.loadChunk("server/chunks/ssr/[root of the server]__8472d4._.js");
-runtime.loadChunk("server/chunks/ssr/_673a39._.css");
+runtime.loadChunk("server/chunks/ssr/node_modules_49e155._.js");
+runtime.loadChunk("server/chunks/ssr/[root of the server]__aa9062._.js");
+runtime.loadChunk("server/chunks/ssr/styles_globals_ff5908.css");
 module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/node_modules/next/error.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/pages/_app.tsx [ssr] (ecmascript)\" } [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
