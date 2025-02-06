f5eb336e0ba106ebd74091018be7f89d3a8d71d0
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-02-06 07:52:06 -0600

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const socialIcons = [
   { icon: FaSoundcloud, link: "https://soundcloud.com/kevinvuu", title: "Soundcloud" }
 ];
 
-const resumePdfPath = 'KevinVu-Resume5.pdf';
+const resumePdfPath = 'KevinVu-Resume6.pdf';
 
 export default function Home() {
   const [isProjectsVisible, setIsProjectsVisible] = useState(true);
