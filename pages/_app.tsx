323bc2b918e22755262dd83b4ea0d456649106b0
Kevin Vu <112991211+amgvu@users.noreply.github.com>
2025-02-08 05:32:30 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Analytics } from "@vercel/analytics/react"
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { GeistSans } from "geist/font/sans";
 
-
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <main className={GeistSans.className}>
-                <Component {...pageProps} />
-                <Analytics />
-        </main>
-    );
+  return (
+    <main className={GeistSans.className}>
+      <Component {...pageProps} />
+      <Analytics />
+    </main>
+  );
 }
